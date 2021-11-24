243839215ac05c5da0ee1dbfa4dc1772066982f9
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-11-24 23:01:05 +0100

    Add the cookie system

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-	"url": "https://fvsg-buende.de/",
-	username: "",
-	password: "",
-};
-
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import * as config from "./config";
-import needle from "needle";
+const config = require('./config.json');
+import needle from 'needle';
+
+import * as querystring from 'query-string';
 
 console.log(config);
 
-
 // Login sourced from https://github.com/dunklesToast/IServ/blob/master/index.js
+const login = querystring.stringify({
+	_username: config.username,
+	_password: config.password,
+});
 
-// needle.post(config.url + "/iserv/login_check", {
+needle.post(config.url + 'iserv/app/login', login, config.needle, function (err, resp) {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	const cookie = resp.cookies;
+	console.log('Logged in');
+	console.log(cookie);
+});
