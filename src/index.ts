bfcc9795a0d4afe34e507130bb723f5afe489d5d
Jonatan_M <60609358+JonatanMGit@users.noreply.github.com>
2021-11-25 20:41:23 +0100

    Save Cookies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 const config = require('./config.json');
-import needle from 'needle';
-
+import * as needle from 'needle';
 import * as querystring from 'query-string';
-
-console.log(config);
+import * as fs from 'fs';
 
 // Login sourced from https://github.com/dunklesToast/IServ/blob/master/index.js
 const login = querystring.stringify({
@@ -11,12 +9,32 @@ const login = querystring.stringify({
 	_password: config.password,
 });
 
-needle.post(config.url + 'iserv/app/login', login, config.needle, function (err, resp) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	const cookie = resp.cookies;
-	console.log('Logged in');
-	console.log(cookie);
-});
+// Login to IServ
+function getCookies() {
+	needle.post(config.url + 'iserv/app/login', login, function (err, resp) {
+		if (err) {
+			console.log(err);
+		} else {
+			fs.writeFileSync('data.json', JSON.stringify(resp.cookies), 'utf-8');
+			console.log('Saved cookies');
+		}
+	});
+}
+if (!fs.existsSync('data.json')) {
+	getCookies();
+}
+
+const cookie = JSON.parse(fs.readFileSync('data.json').toString());
+needle.get(
+	config.url + 'iserv/plan/show/raw/Vertretungsplan%20Sek1%20Heute/subst_001.htm',
+	{ cookies: cookie, follow_max: 0 },
+	function (err, resp) {
+		if (err || resp.statusCode !== 200) {
+			//console.log(err);
+			console.log('Login failed');
+			return;
+		} else {
+			console.log(resp.body);
+		}
+	},
+);
